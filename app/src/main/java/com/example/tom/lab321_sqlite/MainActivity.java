06651c35fbc28851d4fda57356a7a8c0d0b438e9
conchodjen <15520024@gm.uit.edu.vn>
2018-11-06 11:33:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tom/lab321_sqlite/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.tom.lab321_sqlite;
+
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.tom.lab321_sqlite.adapter.DbAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DbAdapter dbAdapter;
+    private Cursor cursor;
+    private List<String> users;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dbAdapter = new DbAdapter(this);
+        dbAdapter.open();
+        dbAdapter.deleteAllUsers();
+
+        for (int i = 0; i < 10; i++) {
+            dbAdapter.createUser("Nguyễn Văn An " + i);
+        }
+
+        users = getData();
+        showData();
+    }
+
+    private List<String> getData() {
+        List<String> users = new ArrayList<>();
+
+        cursor = dbAdapter.getAllUsers();
+        while (cursor.moveToNext()) {
+            users.add(cursor.getString(cursor.getColumnIndex(DbAdapter.KEY_NAME)));
+        }
+
+        return users;
+    }
+
+    private void showData() {
+        ListView lvUser = (ListView) findViewById(R.id.lv_user);
+        ArrayAdapter<String> userAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.item_user, users);
+        lvUser.setAdapter(userAdapter);
+    }
+}
+
+
